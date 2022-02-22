0ca9103fc1eb9cb3ea0403d25325991b51f543f5
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-21 20:18:45 -0700

    Removed consolelog from server.

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import NoteList from './components/NoteList';
+import SideBar from './components/SideBar';
+import { NoteState } from './Context';
+
+function App() {
+  const {
+    notesArray,
+    filterCategory,
+    setNotesArray,
+  } = NoteState();
+
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/note')
+      .then((res) => {
+        setNotesArray(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [setNotesArray]);
+
+  return (
+    <div className="App">
+      <SideBar />
+      <NoteList
+        notesArray={
+            filterCategory === '' ? notesArray.filter((note) => note.text.toLowerCase().includes(searchText))
+              : notesArray.filter((note) => note.text.toLowerCase().includes(searchText))
+            && notesArray.filter((note) => note.category === filterCategory)
+            }
+        setNotesArray={setNotesArray}
+        handleSearchNote={setSearchText}
+      />
+    </div>
+  );
+}
+
+export default App;
