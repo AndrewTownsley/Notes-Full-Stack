8acd9482e3f5cba2a59f980bab6a769d9a04c404
Andrew TOwnsley <atownsley86@gmail.com>
2022-04-01 21:18:33 -0600

    Added compression npm package to server.

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const connectDB = require("./config/db");
 const cors = require("cors");
+const compression = require('compression');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const router = express.Router();
 const path = require('path');
 require("dotenv").config({ path: './env'});
+
+
 const app = express();
+
 const PORT = process.env.PORT || 8000;
+app.use(compression());
 app.use(cors({ origin: true, credentials: true}));
 app.use(express.json({ extended: false }));
 app.use(morgan("dev"));
+
 
 
 const note = require("./routes/note"); 
