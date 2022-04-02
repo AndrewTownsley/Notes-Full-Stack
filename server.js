137b3aeb984f99f66b78a8d0580e6436c133fbc7
Andrew TOwnsley <atownsley86@gmail.com>
2022-04-01 22:19:03 -0600

    gzipping server files.

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ app.use("/", expressStaticGzip("/", {
     enableBrotli: true,
     index: false,
 }));
-function shouldCompress (req, res) {
-    if (req.headers['x-no-compression']) {
-      // don't compress responses with this request header
-      return false
-    }
-  
-    // fallback to standard filter function
-    return compression.filter(req, res)
-  }
 app.use(cors({ origin: true, credentials: true}));
 app.use(express.json({ extended: false }));
 app.use(morgan("dev"));
